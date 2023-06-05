f0786aef73108e20fdc9ba1dcb61720456d40f7d
endlessorhub <anthonyhutchi99@gmail.com>
2023-06-05 19:21:05 +0500

    fixed static files issue

--- teachbeach/settings.py
+++ teachbeach/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 REST_FRAMEWORK = {
